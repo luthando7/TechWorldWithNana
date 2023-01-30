b3e48e801bd35c16b350cc55655fd73e9cd94a5e
Luthando Zaze <luthandozaze@gmail.com>
2023-01-30 11:25:39 +0200

    Build tools exercise done

---
+++ 4 - Build Tools & Package Manager Tools/java-gradle-app/src/main/java/com/example/Application.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+        Logger log = LoggerFactory.getLogger(Application.class);
+        try {
+            String one = args[0];
+            String two = args[2];
+            log.info("Application will start with the parameters {} and {}", one , two);
+        } catch (Exception e) {
+            log.info("No parameters provided");
+        }
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        Logger log = LoggerFactory.getLogger(Application.class);
+        log.info("Java app started");
+    }
+
+    public String getStatus() {
+        return "OK";
+    }
+
+    public boolean getCondition(boolean condition) {
+        return condition;
+    }
+}
